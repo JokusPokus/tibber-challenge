09c1c44a6dae9682bf2cc7c09c1d98b1275d96fa
JokusPokus <jakob.schmitt@code.berlin>
2022-06-28 18:57:56 +0200

    feat: Add view skeleton for handling cleaning jobs

--- robot/views.py
+++ robot/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions
 
-# Create your views here.
+
+class ListUsers(APIView):
+    """
+    View to create an execution instance based on a robot cleaning job.
+    """
+
+    authentication_classes = []
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, format=None):
+        """
+        Accept a cleaning job, and create and return an execution record.
+
+        Expected request body items:
+          - start: a dictionary holding x and y coordinates of the
+            robot's starting position
+          - commands: a list of commands to be executed, with each command
+            specifying the direction to move in and the number of steps.
+        """
+        start = request.data.get('start')
+        commands = request.data.get('commands')
+
+        return Response()
