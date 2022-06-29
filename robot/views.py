06313e021fcaed1493128afe0f56b7331373a556
JokusPokus <jakob.schmitt@code.berlin>
2022-06-29 15:55:02 +0200

    fix: Add correct response type

--- robot/views.py
+++ robot/views.py
@@ -1,7 +1,7 @@
 import timeit
 
 from rest_framework import permissions
-from rest_framework.response import JsonResponse
+from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 
@@ -9,7 +9,7 @@ from .cleanbot import RobotTracker
 from .serializers import ExecutionSerializer
 
 
-class ListUsers(APIView):
+class PostCleaningJob(APIView):
     """
     View to create an execution instance based on a robot cleaning job.
     """
@@ -42,6 +42,6 @@ class ListUsers(APIView):
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=HTTP_200_OK)
+            return Response(serializer.data, status=HTTP_201_CREATED)
 
-        return JsonResponse(serializer.errors, status=HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
