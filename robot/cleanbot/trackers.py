f9fe65eddbbb8302d2f77360d088bcfb20c76653
JokusPokus <jakob.schmitt@code.berlin>
2022-06-29 17:04:06 +0200

    feat: Initialize helper classes

--- robot/cleanbot/trackers.py
+++ robot/cleanbot/trackers.py
@@ -1,7 +1,28 @@
 """
 Tracker classes for robot cleaning jobs.
 """
+from collections import defaultdict
 from typing import Dict, List
+
+
+class CleanedRange:
+    """Represents a range of vertices that have been cleaned."""
+    def __int__(self, start: int, end: int):
+        self.start = start
+        self.end = end
+
+
+class Office:
+    """Represents the office space."""
+    def __init__(self):
+        """
+        self.rows: a dictionary with each key being the y coordinate of the
+            row and its value being a list of CleanedRange objects
+        self.cols: a dictionary with each key being the x coordinate of the
+            column and its value being a list of CleanedRange objects
+        """
+        self.rows = defaultdict(list)
+        self.cols = defaultdict(list)
 
 
 class RobotTracker:
@@ -9,15 +30,17 @@ class RobotTracker:
     Tracks robot cleaning job and calculates the number of vertices cleaned.
     """
     @staticmethod
-    def get_num_of_cleaned_vertices(start: Dict, commands: List[Dict]) -> int:
+    def get_num_of_cleaned_vertices(commands: List[Dict]) -> int:
         """Calculate the number of unique vertices cleaned by the robot.
 
         Args:
-          - start: a dictionary holding x and y coordinates of the
-            robot's starting position
           - commands: a list of commands to be executed, with each command
             specifying the direction to move in and the number of steps.
         """
-        x, y = start['x'], start['y']
+        x, y = 0, 0
 
-        return 0
+        for command in commands:
+            direction = command['direction']
+            steps = command['steps']
+
+
