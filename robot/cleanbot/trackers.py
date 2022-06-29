fd1bcfa52d34a2946c8650f1ec60bede69445c24
JokusPokus <jakob.schmitt@code.berlin>
2022-06-29 19:19:54 +0200

    feat: Add vertex helper class

--- robot/cleanbot/trackers.py
+++ robot/cleanbot/trackers.py
@@ -2,7 +2,15 @@
 Tracker classes for robot cleaning jobs.
 """
 from collections import defaultdict
-from typing import Dict, List
+from dataclasses import dataclass
+from typing import Dict, List, Optional
+
+
+@dataclass
+class Vertex:
+    """Represents a vertex in the 2D office grid."""
+    x: int
+    y: int
 
 
 class CleanedRange:
@@ -14,15 +22,23 @@ class CleanedRange:
 
 class Office:
     """Represents the office space."""
-    def __init__(self):
+    def __init__(self, robot_position: Optional[Vertex] = None):
         """
+        self.robot_position: the robot's current position within the 2D grid
+
         self.rows: a dictionary with each key being the y coordinate of the
             row and its value being a list of CleanedRange objects
-        self.cols: a dictionary with each key being the x coordinate of the
-            column and its value being a list of CleanedRange objects
+
+        self.num_unique_vertices: the number of unique vertices that have
+            been cleaned so far
         """
+        self.robot_position = robot_position or Vertex(0, 0)
         self.rows = defaultdict(list)
-        self.cols = defaultdict(list)
+        self.num_unique_vertices = 0
+
+    def move_robot(self, direction: str, steps: int) -> None:
+        """Move the robot in the specified direction and number of steps."""
+        pass
 
 
 class RobotTracker:
@@ -37,10 +53,12 @@ class RobotTracker:
           - commands: a list of commands to be executed, with each command
             specifying the direction to move in and the number of steps.
         """
-        x, y = 0, 0
+        office = Office()
 
         for command in commands:
             direction = command['direction']
             steps = command['steps']
 
+            office.move_robot(direction, steps)
 
+        return office.num_unique_vertices
