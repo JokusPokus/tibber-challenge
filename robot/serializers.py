7fcd939959f54356c012aeeafa5de6c803a84281
JokusPokus <jakob.schmitt@code.berlin>
2022-06-29 15:52:58 +0200

    feat: Change duration representation

--- robot/serializers.py
+++ robot/serializers.py
@@ -10,3 +10,11 @@ class ExecutionSerializer(serializers.ModelSerializer):
         model = Execution
         fields = ['timestamp', 'commands', 'result', 'duration']
         read_only_fields = ['timestamp']
+
+    def to_representation(self, instance):
+        """
+        Override to_representation to display duration as a formatted string.
+        """
+        representation = super().to_representation(instance)
+        representation['duration'] = format(representation['duration'], '.6f')
+        return representation
