79dab0e0a6013a612e9be3d1c6f95aed2fb98139
JokusPokus <jakob.schmitt@code.berlin>
2022-06-29 15:58:08 +0200

    test: Add API test case

--- robot/tests.py
+++ robot/tests.py
@@ -6,10 +6,28 @@ import json
 
 from django.urls import reverse
 
+from .models import Execution
+
 
 @pytest.mark.django_db
 class TestRobotAPI:
-    def test_create_execution(self, client):
+    def test_execution_is_created(self, client):
+        """An execution instance is created when a valid request is made."""
+        num_executions = Execution.objects.count()
+
+        payload = {
+            'start': {'x': 0, 'y': 0},
+            'commands': [{'direction': 'north', 'steps': 1}]
+        }
+        client.post(
+            reverse('robot:clean'),
+            data=json.dumps(payload),
+            content_type='application/json'
+        )
+
+        assert Execution.objects.count() == num_executions + 1
+
+    def test_execution_data_is_returned(self, client):
         """The expected JSON response with execution data is returned when
         a valid request is made.
         """
