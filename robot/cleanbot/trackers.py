37a164ef5118af1df99e8e30324448c12b232392
JokusPokus <jakob.schmitt@code.berlin>
2022-06-30 11:08:51 +0200

    feat: Add naive tracker implementation

--- robot/cleanbot/trackers.py
+++ robot/cleanbot/trackers.py
@@ -244,3 +244,50 @@ class RobotTracker:
         return sum(
             1 for y in col.members if x not in self.office.rows[y]
         )
+
+
+class SimpleRobotTracker:
+    """Naive implementation of a RobotTracker. Will be very slow for large
+    problems.
+    """
+
+    @staticmethod
+    def get_num_of_cleaned_vertices(commands: List[Dict]) -> int:
+        """Calculate the number of unique vertices cleaned by the robot.
+
+        Save the coordinates of visited vertices in a set and add every
+        single unique vertex the robot passes. The final result is the length
+        of the set after the robot has finished.
+        """
+        robot_pos = Position(0, 0)
+
+        visited = {(0, 0)}
+
+        for command in commands:
+            direction, steps = command['direction'], command['steps']
+
+            if direction == 'east':
+                places = {
+                    (robot_pos.x + i, robot_pos.y)
+                    for i in range(1, steps + 1)
+                }
+            elif direction == 'west':
+                places = {
+                    (robot_pos.x - i, robot_pos.y)
+                    for i in range(1, steps + 1)
+                }
+            elif direction == 'north':
+                places = {
+                    (robot_pos.x, robot_pos.y + i)
+                    for i in range(1, steps + 1)
+                }
+            else:
+                places = {
+                    (robot_pos.x, robot_pos.y - i)
+                    for i in range(1, steps + 1)
+                }
+
+            visited |= places
+            robot_pos.update(direction, steps)
+
+        return len(visited)
